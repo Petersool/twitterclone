0e5b7916625777df9b9371466920412f775c4272
Aydin <iranica@gmail.com>
2021-04-16 14:35:29 +0200

    leftsidebar

--- src/components/LeftSidebar/index.js
+++ src/components/LeftSidebar/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './style.css';
 import TwitterIcon from '@material-ui/icons/Twitter';
 import HomeIcon from '@material-ui/icons/Home';
-import SidebarOption from './SidebarOption';
 import SearchIcon from '@material-ui/icons/Search';
 import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
 import MailOutlineIcon from '@material-ui/icons/MailOutline';
@@ -12,6 +11,14 @@ import PermIdentityIcon from '@material-ui/icons/PermIdentity';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import { Button } from "@material-ui/core";
 
+function SidebarOption({ active, text, Icon }) {
+    return (
+        <div className={`sidebarOption ${active&& 'sidebarOption--active'}`}>
+            <Icon />
+            <h2>{text}</h2>
+        </div>
+    );
+}
 function LeftSidebar() {
   return (
      <div className="MessageList mb-5">
