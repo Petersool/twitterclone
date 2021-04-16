98ad6feec73f3bde4d8775febd4cca0e893b08fa
Alexander Ditz <77445887+AD2348f@users.noreply.github.com>
2021-04-16 12:58:28 +0200

    Display flex and sections

--- src/App.js
+++ src/App.js
@@ -4,18 +4,19 @@ import MessageList from "./components/MessageList";
 import SearchBar from "./components/SearchBar";
 import LeftSideBar from "./components/LeftSidebar";
 import RightSideBar from "./components/RightSidebar";
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <main className="mb-0">
-        {/* <SearchBar /> */}
-        <LeftSideBar />
-        {/* <RightSideBar /> */}
-        <Container>
-          <MessageList />
-        </Container>
-      </main>
+      <div className="mb-0">        
+        {/*<div><SearchBar /> */}
+        <div><LeftSideBar /></div>        
+        <div><Container /></div>
+        <div><MessageList /></div>
+        <div><RightSideBar /></div>
+        <div><Container /></div>
+      </div>
     </div>
   );
 }
---
+++ src/components/RightSidebar/index.js
@@ -0,0 +1,14 @@
+function RightSidebar() {
+    return (
+      <div className="MessageList mb-5">
+        <div>
+          <h1>RightSidebar</h1>
+          <h2>Current user:</h2>
+          <div>Name:</div>
+          <div>ID:</div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default RightSidebar;
